a7591522f0b485a8c9cb3730e2ef082628d25b16
Raymond Burkholder <raymond@burkholder.net>
2016-12-30 16:12:04 -0400

    ryulab - start of ry script

---
+++ ryulab/k01.sh
@@ -0,0 +1,12 @@
+#k01
+ovs-vsctl add-br br0 \
+  -- set bridge br0 fail-mode=secure \
+  -- set-controller br0 tcp:10.10.103.2
+ovs-vsctl set bridge br0 protocols=OpenFlow14 \
+
+ovs-vsctl add-port br0 enp0s9
+ovs-vsctl add-port br0 enp0s10
+ovs-vsctl add-port br0 enp0s16
+ovs-vsctl add-port br0 enp0s17
+ovs-vsctl add-port br0 enp0s18
+
